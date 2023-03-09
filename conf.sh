60a5b3e065e328412113f32f457ff1ca08ce1201
Bakarseck <seck.bakar@ugb.edu.sn>
2023-03-08 21:22:11 -0800

    talent-01

---
+++ conf.sh
@@ -0,0 +1,3 @@
+git config --global credential.helper store
+git config --global user.email "seck.bakar@ugb.edu.sn"
+git config --global user.name "Bakarseck"
---
+++ push.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "talent-01"
+git push
+pit pull
