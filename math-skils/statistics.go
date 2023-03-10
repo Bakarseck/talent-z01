5ec9baa11dd4873ef0fcbc081e964b4a85a2bc5a
Bakarseck <seck.bakar@ugb.edu.sn>
2023-03-10 11:13:46 -0800

    talent-01

--- math-skils/statistics.go
+++ math-skils/statistics.go
@@ -3,17 +3,86 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"strconv"
 	"strings"
-	"math"
 )
 
-func StandardDeviation(s string) int {
-	return int(math.Sqrt(float64(Variance(s))))
+func Average(tabFloat []float64) float64 {
+
+	sum := 0.0
+	for _, v := range tabFloat {
+		sum += v
+	}
+	return sum / float64(len(tabFloat))
 }
 
-func sortIntegers(numbers []int) {
+func Median(tabFloat []float64) float64 {
+
+	sortFloat64(tabFloat)
+
+	length := len(tabFloat)
+
+	middle := len(tabFloat) / 2
+
+	median := 0.0
+
+	if length % 2 == 0 {
+		middleLeft := tabFloat[middle -1]
+		middleRight := tabFloat[middle]
+		median = (middleLeft + middleRight) / 2
+	} else if length % 2 != 0{
+		median = tabFloat[middle]
+	}
+	return median
+}
+
+func Variance(tabFloat []float64, mean float64) float64 {
+
+	sum := 0.0
+
+	for _, v := range tabFloat {
+		sum += math.Pow(v - mean, 2)
+	}
+
+	return sum / float64(len(tabFloat))
+}
+
+func StandardDeviation(tabFloat []float64, mean float64) float64 {
+ 	return math.Sqrt(float64(Variance(tabFloat, mean)))
+}
+
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	arguments := os.Args[1:]
+
+	sampleFile, err := ioutil.ReadFile(arguments[0])
+
+	check(err)
+
+	sampleFileString := string(sampleFile)
+
+	tabFloat := stringToFloat(sampleFileString)
+
+	average := Average(tabFloat)
+	median := Median(tabFloat)
+	variance := Variance(tabFloat, average)
+	std := StandardDeviation(tabFloat, average)
+	fmt.Printf("Mean: %v\n", int(math.Round(average)))
+	fmt.Printf("Median: %v\n", int(math.Round(median)))
+	fmt.Printf("Variance: %v\n", int(math.Round(variance)))
+	fmt.Printf("Standard deviation: %v\n", int(math.Round(std)))
+}
+
+func sortFloat64(numbers []float64) {
 
 	for i := 0; i < len(numbers); i++ {
 		for j := i + 1; j < len(numbers); j++ {
@@ -26,117 +95,17 @@ func sortIntegers(numbers []int) {
 	}
 }
 
-func AverageFLoat(s string) float64 {
+func stringToFloat(str string) []float64 {
+
+	s := strings.TrimSpace(str)
 
 	numbersString := strings.Split(s, "\n")
 
-	var numbers []int
+	var numbers []float64
 
 	for _, element := range numbersString {
 		n, _ := strconv.Atoi(element)
-		numbers = append(numbers, n)
+		numbers = append(numbers, float64(n))
 	}
-
-	somme := 0.0
-	for _, number := range numbers {
-		somme += float64(number)
-	}
-
-	return float64(somme/float64(len(numbers)))
-}
-
-func Variance(s string) int {
-
-	numbersString := strings.Split(s, "\n")
-
-	var numbers []int
-
-	for _, element := range numbersString {
-		n, _ := strconv.Atoi(element)
-		numbers = append(numbers, n)
-	}
-
-	var squareNumbers []float64
-	for _, number := range numbers {
-
-		squareNumbers = append(squareNumbers, math.Pow( (float64(number) - float64(AverageFLoat(s))), float64(2)))
-	}
-
-	sommeSquareNumber := 0.0
-
-	for _, number := range squareNumbers {
-		sommeSquareNumber += number
-	}
-	return int(sommeSquareNumber / float64(len(numbers)))
-}
-
-func Average(s string) int {
-
-	numbersString := strings.Split(s, "\n")
-
-	var numbers []int
-
-	for _, element := range numbersString {
-		n, _ := strconv.Atoi(element)
-		numbers = append(numbers, n)
-	}
-
-	somme := 0
-	for _, number := range numbers {
-		somme += number
-	}
-
-	return int(somme/len(numbers))
-}
-
-func Median(s string) int {
-	
-	numbersString := strings.Split(s, "\n")
-
-	var numbers []int
-
-	for _, element := range numbersString {
-		n, _ := strconv.Atoi(element)
-		numbers = append(numbers, n)
-	}
-
-	sortIntegers(numbers)
-
-	median := 0
-
-	if len(numbers) % 2 == 0 {
-		median = int(numbers[(len(numbers) / 2) - 1])
-	} else {
-		median = int(numbers[(len(numbers) / 2)])
-	}
-
-	return median
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func main() {
-	
-	arguments := os.Args[1:]
-
-	sampleFile, err := ioutil.ReadFile(arguments[0])
-
-	check(err)
-
-	sampleFileString := string(sampleFile)
-
-	average := Average(sampleFileString)
-	median := Median(sampleFileString)
-	variance := Variance(sampleFileString)
-	std := StandardDeviation(sampleFileString)
-	fmt.Println(std)
-	floatAverage := AverageFLoat(sampleFileString)
-	fmt.Println(floatAverage)
-	fmt.Println(variance)
-	fmt.Println(median)
-	fmt.Println(average)
+	return numbers
 }
